555239f30ad967fca179e52035a1b9a426f6831b
w00ye0l <dnduf158@gmail.com>
2025-06-09 20:16:09 +0900

    fix: supabase 설정 수정

--- src/types/schemas/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./profile";
-export * from "./device";
-export * from "./rental";
-
-// 공통으로 사용되는 타입들
-// export * from './common';
--- src/types/schemas/profile.ts
+++
@@ -1,7 +0,0 @@
-import { type Database } from "../supabase";
-
-export type Profile = Database["public"]["Tables"]["profiles"]["Row"];
-export type ProfileInsert = Database["public"]["Tables"]["profiles"]["Insert"];
-export type ProfileUpdate = Database["public"]["Tables"]["profiles"]["Update"];
-
-export type UserRole = Database["public"]["Enums"]["user_role"];
